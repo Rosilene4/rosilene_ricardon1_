8c5e140fbd028a06db2311fc833ded770669d491
Rosilene4 <129714743+Rosilene4@users.noreply.github.com>
2025-06-20 12:09:11 -0300

    Create Livro.js

---
+++ models/Livro.js
@@ -0,0 +1,20 @@
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('./database');
+
+const Livro = sequelize.define('Livro', {
+    titulo: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    autor: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    ano: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+});
+
+module.exports = Livro;
